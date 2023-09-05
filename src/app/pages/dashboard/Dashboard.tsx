b3eb06c9024b946c6bd4ff5dda2777ee61f14af3
Guilhermemot <Guilhermemapurunga13@gmail.com>
2023-09-04 23:19:33 -0300

    Aula 11: useRef com objetos simples

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,16 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
+
+    const couterRef = useRef({ counter: 0});
+
     return (
         <div>
             <p>Dashboard</p>
+            <p>Contador: {couterRef.current.counter}</p>
+            <button onClick={() => couterRef.current.counter++}>Somar</button>
+            <button onClick={() => console.log(couterRef.current.counter++)}>Somar</button>
             <Link to="/Entrar">Login</Link>
         </div>
     );
