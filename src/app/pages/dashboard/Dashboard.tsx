b013ccb3c59806bd37ec41dddba8382ac30b43d4
Guilhermemot <Guilhermemapurunga13@gmail.com>
2023-10-11 00:21:57 -0300

    Aula 18

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -5,16 +5,17 @@ import { useUsuarioLogado } from '../../shared/hooks';
 export const Dashboard = () => {
 
     const couterRef = useRef({ counter: 0});
-    const usuarioLogadoContext = useUsuarioLogado();
+    const { nomeDoUsuario, logout} = useUsuarioLogado();
     //Outra opção ^ = const { nomeDoUsuario} = useContext(UsuarioLogadoContext);
 
     return (
         <div>
             <p>Dashboard</p>
-            <p>{usuarioLogadoContext.nomeDoUsuario}</p>
+            <p>{nomeDoUsuario}</p>
             <p>Contador: {couterRef.current.counter}</p>
             <button onClick={() => couterRef.current.counter++}>Somar</button>
             <button onClick={() => console.log(couterRef.current.counter++)}>Somar</button>
+            <button onClick={logout}>Logout</button>
             <Link to="/Entrar">Login</Link>
         </div>
     );
--- src/app/shared/contexts/UsuarioLogado.tsx
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -1,8 +1,10 @@
-import React, { createContext } from "react";
+import React, { createContext, useCallback } from "react";
 
 
 interface IUsuarioLogadoContextData{
     nomeDoUsuario: string;
+    logout: () => void;
+    
 }
 export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} as IUsuarioLogadoContextData);
 
@@ -10,10 +12,12 @@ interface IUsuarioLogadoProviderProps{
     children: React.ReactNode
 }
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProviderProps> = ({ children }) =>{
-
+    const handleLogout = useCallback(() =>{
+        console.log('Logout executou')
+    }, []);
 
     return(
-        <UsuarioLogadoContext.Provider value={{nomeDoUsuario: 'Lucas'}}>
+        <UsuarioLogadoContext.Provider value={{nomeDoUsuario: 'Lucas', logout: handleLogout}}>
             {children}
         </UsuarioLogadoContext.Provider>
     );
