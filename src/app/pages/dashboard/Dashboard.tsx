bed5bc90f8397f0ef22ddbaeff2b85b0ee502291
Guilhermemot <guilhermemapurunga13@gmail.com>
2023-10-16 18:04:52 -0300

    Aulas 18, 19, 20, 21.

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,22 +1,63 @@
-import { useRef } from 'react';
-import { Link } from 'react-router-dom';
-import { useUsuarioLogado } from '../../shared/hooks';
+import { useCallback, useState } from 'react';
+
+interface IlistItem{
+    title: string;
+    isSelected: boolean;
+}
 
 export const Dashboard = () => {
+    const [lista, setLista] = useState<IlistItem[]>([]);
 
-    const couterRef = useRef({ counter: 0});
-    const { nomeDoUsuario, logout} = useUsuarioLogado();
-    //Outra opção ^ = const { nomeDoUsuario} = useContext(UsuarioLogadoContext);
+    const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if(e.key === 'Enter'){
+            if (e.currentTarget.value.trim().length === 0) return;
+
+            const value = e.currentTarget.value;
+
+            e.currentTarget.value = '';
+
+            setLista((oldLista) => {
+
+                if(oldLista.some((listItem) => listItem.title === value)) return oldLista;
+
+                return[...oldLista, 
+                
+                    {
+                        title: value,
+                        isSelected: false,
+                    }
+                ];
+            });
+        }
+
+    }, [lista]);
 
     return (
         <div>
-            <p>Dashboard</p>
-            <p>{nomeDoUsuario}</p>
-            <p>Contador: {couterRef.current.counter}</p>
-            <button onClick={() => couterRef.current.counter++}>Somar</button>
-            <button onClick={() => console.log(couterRef.current.counter++)}>Somar</button>
-            <button onClick={logout}>Logout</button>
-            <Link to="/Entrar">Login</Link>
+            <p>Lista</p>
+            <input 
+                onKeyDown={handleInputKeyDown}
+            />
+            <p>{lista.filter((listItem) => listItem.isSelected).length}</p>
+            <ul>
+                {lista.map((listItem) => {
+                    return <li key={listItem.title}>
+                        <input type="checkbox" checked={listItem.isSelected} onChange={() => {
+                            setLista(oldLista =>{
+                                return oldLista.map(oldlistItem => {
+                                    const newIsSelected = oldlistItem.title === listItem.title? !oldlistItem.isSelected: oldlistItem.isSelected;
+                                    return{
+                                        ...oldlistItem,
+                                        isSelected: newIsSelected,
+                                    };
+                                });
+                            });
+                        }}/>
+                        {listItem.title}
+                        </li>;
+                })}
+            </ul>
+
         </div>
     );
 }
--- src/app/shared/contexts/UsuarioLogado.tsx
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback } from "react";
+import React, { createContext, useCallback, useEffect, useState } from "react";
 
 
 interface IUsuarioLogadoContextData{
@@ -12,6 +12,15 @@ interface IUsuarioLogadoProviderProps{
     children: React.ReactNode
 }
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProviderProps> = ({ children }) =>{
+
+    let [nome, setNome] = useState('');
+
+    useEffect(() =>{
+        setTimeout(() =>{
+            setNome('Lucas');
+        }, 1000);
+    });
+
     const handleLogout = useCallback(() =>{
         console.log('Logout executou')
     }, []);
