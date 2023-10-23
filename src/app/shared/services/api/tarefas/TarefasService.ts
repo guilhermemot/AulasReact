9b9179a79c502a7b79a79e4670d9d741daa44611
Guilhermemot <guilhermemapurunga13@gmail.com>
2023-10-23 17:24:15 -0300

    aula 25: comunicação da API

--- src/app/shared/services/api/tarefas/TarefasService.ts
+++ src/app/shared/services/api/tarefas/TarefasService.ts
@@ -1,7 +1,7 @@
 import { ApiException } from "../ApiException";
 import { Api } from "../ApiConfig";
 
-interface ITarefa{
+export interface ITarefa{
     id: number;
     title: string;
     isCompleted: boolean;
